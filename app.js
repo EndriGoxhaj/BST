12f840528c72a30a5647010697b82024832cd0e8
GEndr1 <goxhajendri5@gmail.com>
2025-01-25 21:36:41 +0100

    Added Insert, Delete, and find functions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(data, left, right) {
+  constructor(data, left = null, right = null) {
     this.data = data;
     this.left = left;
     this.right = right;
@@ -24,6 +24,64 @@ class BST {
   }
   returnBuildTree(array = this.array) {
     this.root = this.buildTree(array, 0, array.length - 1);
+  }
+  find(value, root = this.root) {
+    if (!root) return null;
+    if (value == root.data) return root;
+    if (value > root.data) {
+      this.find(value, (root = root.right));
+    } else {
+      this.find(value, (root = root.left));
+    }
+  }
+  insert(value, node = this.root) {
+    if (!this.root) {
+      this.root = new Node(value);
+      return;
+    }
+    if (value === node.data) return;
+    if (value < node.data) {
+      if (!node.left) {
+        node.left = new Node(value);
+      } else {
+        this.insert(value, node.left);
+      }
+    } else {
+      if (!node.right) {
+        node.right = new Node(value);
+      } else {
+        this.insert(value, node.right);
+      }
+    }
+  }
+  getSuccessor(node) {
+    node = node.right;
+    while (node && node.left) {
+      node = node.left;
+    }
+    return node;
+  }
+  delete(value, node = this.root) {
+    if (node === null) {
+      return null;
+    }
+    if (value < node.data) {
+      node.left = this.delete(value, node.left);
+    } else if (value > node.data) {
+      node.right = this.delete(value, node.right);
+    } else {
+      if (node.left === null) {
+        return node.right;
+      }
+      if (node.right === null) {
+        return node.left;
+      } else {
+        let succ = this.getSuccessor(node);
+        node.data = succ.data;
+        node.right = this.delete(succ.data, node.right);
+      }
+    }
+    return node;
   }
 
   merge(a, b, c = [], j = 0, i = 0) {
@@ -51,7 +109,16 @@ class BST {
       return this.merge(a, b, c, j, i);
     }
   }
-
+  mergeSort(array) {
+    if (array.length <= 1) {
+      return array;
+    } else {
+      const m = Math.floor(array.length / 2);
+      const left = this.mergeSort(array.slice(0, m));
+      const right = this.mergeSort(array.slice(m));
+      return this.merge(left, right);
+    }
+  }
   prettyPrint(node = this.root, prefix = "", isLeft = true) {
     if (node === null) {
       return;
@@ -68,19 +135,10 @@ class BST {
       this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   }
-
-  mergeSort(array) {
-    if (array.length <= 1) {
-      return array;
-    } else {
-      const m = Math.floor(array.length / 2);
-      const left = this.mergeSort(array.slice(0, m));
-      const right = this.mergeSort(array.slice(m));
-      return this.merge(left, right);
-    }
-  }
 }
 
 let array = new BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 array.returnBuildTree();
+array.prettyPrint();
+array.delete(67);
 array.prettyPrint();
